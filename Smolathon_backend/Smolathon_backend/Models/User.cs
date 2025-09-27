c238c06640a2019cc2b9946c9cf13c4b1b308773
darina <darinakokoreva763@gmail.com>
2025-09-27 16:44:25 +0300

    1st commit all files

---
+++ Smolathon_backend/Smolathon_backend/Models/User.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Smolathon_backend.Models
+{
+    [Table("users")] // Указываем имя таблицы в БД
+    public class User
+    {
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Column("password_hash")]
+        public string PasswordHash { get; set; }
+
+        [Column("role")]
+        public string Role { get; set; }
+
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
+
